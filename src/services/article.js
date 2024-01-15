8a4e11a1e43b4b7425356dada8aa94b350d2f3f9
rencdr <demirerencan@outlook.com>
2024-01-15 23:26:49 +0300

    refactoring

--- src/services/article.js
+++ src/services/article.js
@@ -5,10 +5,10 @@ const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'API-URL',
+        baseUrl: 'API_URL',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', rapidApiKey);
-            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+            headers.set('X-RapidAPI-Host', 'API_URL');
 
             return headers;
         },
